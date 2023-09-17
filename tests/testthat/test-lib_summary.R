3b2adf28fd52ffdfdca8ec4c934fc7afc646558b
Mutaz Jaber <jaber038@umn.edu>
2023-09-17 14:30:55 -0500

    Add tests to lib_summary

---
+++ tests/testthat/test-lib_summary.R
@@ -0,0 +1,17 @@
+test_that("lib_summary returns expected result", {
+  res <- lib_summary()
+  # we want to test the class since we expect data.frame
+  expect_s3_class(res, "data.frame")
+  # We want to make sure that lib_summary function returns two columns.
+  expect_equal(ncol(res), 2)
+  # Check columns name
+  expect_equal(names(res), c("Library", "n_packages"))
+  # Check Library column
+  expect_type(res$Library, "character")
+  # Check n_packages column is integer
+  expect_type(res$n_packages, "integer")
+})
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), "unused argument")
+})
