300767127f401e1f5918f70d47e0b04ff9afc408
Mutaz Jaber <jaber038@umn.edu>
2023-09-17 12:04:35 -0500

    Add GitHub links to DESCRIPTION

---
+++ mynotes.R
@@ -0,0 +1,23 @@
+# pak::pak("devtools")
+
+library(devtools)
+# use_git_config(
+#   user.name = "Mutaz Jaber",
+#   user.email = "jaber038@umn.edu"
+# )
+
+#' git_vaccinate() --> Read more about this.
+#' git_sitrep() --> You will get more information about GitHub integration.
+#' use_git()
+#' usethis::use_devtools() --> will automatically load devtools in R session
+#'
+#' To create functions in R/ directory:
+#' usethis::use_r("lib-summary")
+#'
+#' To test your functions: Cmd + Shift + L (Key shortcut)
+#' devtools::load_all()
+#' Check your package, do this as often.
+#' check() --> R CMD check
+#' use_mit_license() --> There are different options, read more about them.
+#' Ctrl + . is good Rstudio fuzzy finder
+#' To ignore files during the building, look at .Rbuildignore
